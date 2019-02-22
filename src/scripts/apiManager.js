68972dda80f55b6c6e19d91384ee7681f724c204
Panya Farnette <naienko@gmail.com>
2019-02-22 11:48:38 -0600

    starter data

---
+++ src/scripts/apiManager.js
@@ -0,0 +1,21 @@
+/*
+Author: Panya
+Task: connect to api and manipulate data there
+*/
+
+const apiManager = {
+    get: () => {
+
+    },
+    create: () => {
+
+    },
+    edit: () => {
+
+    },
+    delete: () => {
+
+    }
+}
+
+export default apiManager;
