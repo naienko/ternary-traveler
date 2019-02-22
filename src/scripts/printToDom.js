78f8c9ad9100e5abc449830b3570e78fc1a29ad4
Panya Farnette <naienko@gmail.com>
2019-02-22 12:16:35 -0600

    created add and replace methods on printToDom object in printToDom.js; created placeFactory method on htmlFactory object in htmlfactory.js

---
+++ src/scripts/printToDom.js
@@ -0,0 +1,16 @@
+/*
+Author: Panya
+Task: display data on the DOM
+*/
+
+const printToDom = {
+    // given a method returning a string of HTML, add it to the DOM
+    add: (where, what) => {
+        document.querySelector(where).innerHTML += what;
+    },
+    replace: (where, what) => {
+        document.querySelector(where).innerHTML = what;
+    }
+};
+
+export default printToDom;
